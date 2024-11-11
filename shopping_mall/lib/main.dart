7f0572573e3fa2d0079b4ba3c2e000a2431f829f
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-11-11 21:06:51 +0900

    Shopping App 세팅

---
+++ shopping_mall/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
