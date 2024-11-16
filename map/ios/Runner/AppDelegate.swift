2e7e11c5f019c86833882022922509cc6af2f25c
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-11-16 13:23:23 +0900

    Chool check 프로젝트 google map 세팅

--- map/ios/Runner/AppDelegate.swift
+++ map/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,22 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+       // Flutter에서 API 키를 전달받기 위한 MethodChannel 설정
+    let controller = window?.rootViewController as! FlutterViewController
+    let apiKeyChannel = FlutterMethodChannel(name: "com.example.map/apiKey", binaryMessenger: controller.binaryMessenger)
+    
+    // MethodChannel에서 'setApiKey' 호출을 수신하고, API 키를 설정
+    apiKeyChannel.setMethodCallHandler { (call: FlutterMethodCall, result: FlutterResult) in
+      if call.method == "setApiKey" {
+        if let apiKey = call.arguments as? String {
+          GMSServices.provideAPIKey(apiKey)
+          result(nil) // 성공적으로 설정되었음을 알림
+        } else {
+          result(FlutterError(code: "INVALID_API_KEY", message: "API Key is invalid", details: nil))
+        }
+      }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
