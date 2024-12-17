61a29997d33cc2b2a5d2e08d4828ead2638a5d97
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-12-17 23:25:22 +0900

    theme, color 설정 및 splash 추가

---
+++ movie/lib/presentation/splash/bloc/splash_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie/presentation/splash/bloc/splash_state.dart';
+
+class SplashCubit extends Cubit<SplashState> {
+  // 초기화
+  SplashCubit() : super(DisplaySplash());
+
+  void appStarted() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    emit(Authenticated());
+  }
+}
