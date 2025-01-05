fffeea1169a8799dcc6c6b916659ba19a7b50160
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2025-01-05 22:52:18 +0900

    TabBarView 추가

--- middle_class/lib/common/view/root_tab.dart
+++ middle_class/lib/common/view/root_tab.dart
@@ -9,37 +9,73 @@ class RootTab extends StatefulWidget {
   State<RootTab> createState() => _RootTabState();
 }
 
-class _RootTabState extends State<RootTab> {
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
   int index = 0;
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 4, vsync: this);
+
+    controller.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListener);
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      index = controller.index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-        title: '딜리버리',
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: PRIMARY_COLOR,
-          unselectedItemColor: BODY_TEXT_COLOR,
-          selectedFontSize: 10,
-          unselectedFontSize: 10,
-          type: BottomNavigationBarType.fixed,
-          onTap: (int index) {
-            setState(() {
-              this.index = index;
-            });
-          },
-          currentIndex: index,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.house_outlined), label: '홈'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.fastfood_outlined), label: '음식'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.receipt_long_outlined), label: '주문'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_outlined), label: '프로필'),
-          ],
-        ),
-        child: const Center(
-          child: Text('Root'),
-        ));
+      title: '딜리버리',
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          controller.animateTo(index);
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.house_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined), label: '음식'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outlined), label: '프로필'),
+        ],
+      ),
+      child: TabBarView(
+        // 좌우 스크롤 막음
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: [
+          Container(
+            child: const Text('홈'),
+          ),
+          Container(
+            child: const Text('음식'),
+          ),
+          Container(
+            child: const Text('주문'),
+          ),
+          Container(
+            child: const Text('프로필'),
+          ),
+        ],
+      ),
+    );
   }
 }
