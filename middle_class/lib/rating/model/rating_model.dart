9a05e229ea917163a1800721199eaad68d36a265
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2025-02-03 22:42:21 +0900

    rating model 수정 - userModel 분리, listPathsToUrls 추가

--- middle_class/lib/rating/model/rating_model.dart
+++ middle_class/lib/rating/model/rating_model.dart
@@ -1,12 +1,15 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:middle_class/common/utils/data_utils.dart';
+import 'package:middle_class/user/model/user_model.dart';
 
 part 'rating_model.g.dart';
 
 @JsonSerializable()
 class RatingModel {
   final String id;
+  @JsonKey(fromJson: DataUtils.listPathsToUrls)
   final List<String> imgUrls;
-  final User user;
+  final UserModel user;
   final String content;
   final int rating;
 
@@ -19,21 +22,4 @@ class RatingModel {
 
   factory RatingModel.fromJson(Map<String, dynamic> json) =>
       _$RatingModelFromJson(json);
-}
-
-@JsonSerializable()
-class User {
-  final String id;
-  final String username;
-  final String imageUrl;
-
-  User({
-    required this.id,
-    required this.imageUrl,
-    required this.username,
-  });
-
-  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-
-  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
--- middle_class/lib/rating/model/rating_model.g.dart
+++ middle_class/lib/rating/model/rating_model.g.dart
@@ -9,10 +9,9 @@ part of 'rating_model.dart';
 RatingModel _$RatingModelFromJson(Map<String, dynamic> json) => RatingModel(
       id: json['id'] as String,
       content: json['content'] as String,
-      imgUrls:
-          (json['imgUrls'] as List<dynamic>).map((e) => e as String).toList(),
+      imgUrls: DataUtils.listPathsToUrls(json['imgUrls'] as List<String>),
       rating: (json['rating'] as num).toInt(),
-      user: User.fromJson(json['user'] as Map<String, dynamic>),
+      user: UserModel.fromJson(json['user'] as Map<String, dynamic>),
     );
 
 Map<String, dynamic> _$RatingModelToJson(RatingModel instance) =>
@@ -22,16 +21,4 @@ Map<String, dynamic> _$RatingModelToJson(RatingModel instance) =>
       'user': instance.user,
       'content': instance.content,
       'rating': instance.rating,
-    };
-
-User _$UserFromJson(Map<String, dynamic> json) => User(
-      id: json['id'] as String,
-      imageUrl: json['imageUrl'] as String,
-      username: json['username'] as String,
-    );
-
-Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
-      'id': instance.id,
-      'username': instance.username,
-      'imageUrl': instance.imageUrl,
     };
